1549083d40670de6ffc6f787a57312694458793c
zurdooo <a.ramos37@Hotmail.com>
2025-09-07 02:54:26 -0400

    Notes added to tradebot and created notebooks with yfinance sample

---
+++ Stonks/trade_bot/tradebot.py
@@ -0,0 +1,14 @@
+"""
+This contains the logic for future implementation of a trading bot.
+We should also add a smart way to log its activity. Measure its performance.
+
+I/O functions
+Buy, Sell, Hold, Buy amount, Sell amount, Stop loss, Take profit
+
+Libraries to consider:
+-Alpaca
+-yfinance
+-PyTorch
+-NumPy
+-Backtrader
+"""
